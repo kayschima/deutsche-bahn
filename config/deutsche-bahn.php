f7f2a315824f86e3f7e9540153e5b1cad8561004
Kay Markschies <kayschima@web.de>
2020-10-11 23:00:40 +0200

    Changes to package class

--- config/deutsche-bahn.php
+++ config/deutsche-bahn.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'deutsche_bahn_api_token' => env('DEUTSCHE_BAHN_API_TOKEN','YOUR_API_TOKEN'),
+    'api-token' => env('DEUTSCHE_BAHN_API_TOKEN', 'YOUR_API_TOKEN'),
 ];
--- src/DeutscheBahn.php
+++ src/DeutscheBahn.php
@@ -5,19 +5,89 @@ namespace Kayschima\DeutscheBahn;
 use DateTime;
 use Illuminate\Support\Facades\Http;
 
+/**
+ * Class DeutscheBahn
+ *
+ * @package Kayschima\DeutscheBahn
+ */
 class DeutscheBahn
 {
-    public function getArrivals(string $station_id, DateTime $datetime = null)
+    /**
+     * @param  string  $station_id
+     * @param  DateTime|null  $datetime
+     * @param  bool  $details
+     *
+     * @return array|mixed
+     */
+    public function getArrivals(string $station_id, DateTime $datetime = null, bool $details = false)
     {
         $response = Http::withToken(config('deutsche-bahn.api-token'))
-        ->get(
-            'https://api.deutschebahn.com/fahrplan-plus/v1/arrivalBoard/'. $station_id,
-            [
-            'date' => $datetime ? $datetime->format('Y-m-d\TH:i') : ''
-            ]
-        )
-        ->json();
+                        ->get(
+                            'https://api.deutschebahn.com/fahrplan-plus/v1/arrivalBoard/'.$station_id,
+                            [
+                                'date' => $datetime ? $datetime->format('Y-m-d\TH:i') : ''
+                            ]
+                        );
 
-        return collect($response);
+        $returnArray = [];
+
+        if ( $response->successful()) {
+            $returnArray = $response->json();
+
+            if ($details) {
+                foreach ($returnArray as $key => $item) {
+                    $returnArray[$key]['details'] = $this->addDetails($item['detailsId']);
+                }
+            }
+        }
+        return $returnArray;
+    }
+
+    /**
+     * @param  string  $station_id
+     * @param  DateTime|null  $datetime
+     * @param  bool  $details
+     *
+     * @return array|mixed
+     */
+    public function getDepartures(string $station_id, DateTime $datetime = null, bool $details = false)
+    {
+        $response = Http::withToken(config('deutsche-bahn.api-token'))
+                        ->get(
+                            'https://api.deutschebahn.com/fahrplan-plus/v1/departureBoard/'.$station_id,
+                            [
+                                'date' => $datetime ? $datetime->format('Y-m-d\TH:i') : ''
+                            ]
+                        );
+
+        $returnArray = [];
+
+        if ( $response->successful()) {
+            $returnArray = $response->json();
+
+            if ($details) {
+                foreach ($returnArray as $key => $item) {
+                    $returnArray[$key]['details'] = $this->addDetails($item['detailsId']);
+                }
+            }
+        }
+        return $returnArray;
+    }
+
+    /**
+     * @param  string  $details_id
+     *
+     * @return array|mixed
+     */
+    private function addDetails(string $details_id)
+    {
+        $response = Http::withToken(config('deutsche-bahn.api-token'))
+                        ->get('https://api.deutschebahn.com/fahrplan-plus/v1/journeyDetails/'. rawurlencode($details_id));
+
+        if ( ! $response->successful()) {
+            return [];
+        } else {
+            return $response->json();
+        }
     }
 }
